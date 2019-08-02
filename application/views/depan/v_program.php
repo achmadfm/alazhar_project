ee4da6b3dfb5a627020a6b31a42cc903f8373c90
DESKTOP-3GS0CMR\Yatahagane <achmadfm18@gmail.com>
2019-08-02 08:58:48 +0800

    tambahan real time chat

--- application/views/depan/v_program.php
+++ application/views/depan/v_program.php
@@ -187,17 +187,18 @@
 
 		<!-- Theme Initialization Files -->
 		<script src="<?php echo base_url()?>template/js/theme.init.js"></script>
-
-		<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information.
-		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-			ga('create', 'UA-12345678-1', 'auto');
-			ga('send', 'pageview');
+		<!--Start of Tawk.to Script-->
+		<script type="text/javascript">
+		var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+		(function(){
+		var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+		s1.async=true;
+		s1.src='https://embed.tawk.to/5d4296497d27204601c8b58e/default';
+		s1.charset='UTF-8';
+		s1.setAttribute('crossorigin','*');
+		s0.parentNode.insertBefore(s1,s0);
+		})();
 		</script>
-		 -->
+		<!--End of Tawk.to Script-->
 	</body>
 </html>
